f8867b831446aee7f3b870d09437412b421e9f9e
HHxxxmmm <2415835116@qq.com>
2025-07-01 18:12:48 +0800

    feat: 初始化 Sptingboot(Maven) 后端项目                - 创建 Maven 项目配置文件 pom.xml                - 添加 Spring Boot、MyBatis、Spring Cloud 等依赖                - 创建项目主类 TechPrototypeApplication                - 添加单元测试类 TechPrototypeApplicationTests                - 创建应用配置文件 application.properties                - 配置 Maven Wrapper 文件和脚本                - 添加 Git 相关配置文件

---
+++ TechPrototype/src/test/java/com/example/techprototype/TechPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechPrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
