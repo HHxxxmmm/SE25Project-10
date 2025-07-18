cba1a5aa4f1bbd33d4a973f7f5a787c5e1daa7f7
Kiristugu <141491635+Kiristugu@users.noreply.github.com>
2025-07-18 12:36:28 +0800

    Update App.js

--- UIPrototype/mini-12306/src/App.js
+++ UIPrototype/mini-12306/src/App.js
@@ -1,14 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout } from 'antd';
 import { AppHeader, AppSider } from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
 import { routes } from './routes';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearChangeTicket } from './store/actions/changeTicketActions';
 import './App.css';
 
 const { Content } = Layout;
 
 export default function App() {
     const [siderCollapsed, setSiderCollapsed] = useState(false);
+    const dispatch = useDispatch();
+    const changeTicketState = useSelector(state => state.changeTicket);
+
+    // 全局监听应用关闭事件，清理改签状态
+    useEffect(() => {
+        const handleBeforeUnload = () => {
+            if (changeTicketState.isChanging) {
+                dispatch(clearChangeTicket());
+            }
+        };
+
+        const handleUnload = () => {
+            if (changeTicketState.isChanging) {
+                dispatch(clearChangeTicket());
+            }
+        };
+
+        window.addEventListener('beforeunload', handleBeforeUnload);
+        window.addEventListener('unload', handleUnload);
+
+        return () => {
+            window.removeEventListener('beforeunload', handleBeforeUnload);
+            window.removeEventListener('unload', handleUnload);
+        };
+    }, [changeTicketState.isChanging, dispatch]);
 
     return (
         <Layout className="app-layout">
